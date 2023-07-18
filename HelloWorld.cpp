22657b09c89d109a1258aeb7621a56d3b29f8e16
Lucía Gil <luciagilgracia03@gmail.com>
2023-07-18 14:55:20 +0200

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+std::string allowPause;
+
+int main() {
+	
+	std::cout << "Hello World" << std::endl;
+
+	//I'm using this to allow the program time to display
+	//"Hello World" without closing, and it's the only
+	//way I know to do it right now.
+	std::cin >> allowPause;
+
+	return 0;
+}
